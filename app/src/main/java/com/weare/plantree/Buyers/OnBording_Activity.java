afb3ffc9eed901ffec4a3a339297cf6e4ed99c57
Anand <83801261+anand45151@users.noreply.github.com>
2024-08-04 20:18:22 +0530

    OnBording Screen Has Been Done Now

---
+++ app/src/main/java/com/weare/plantree/Buyers/OnBording_Activity.java
@@ -0,0 +1,16 @@
+package com.weare.plantree.Buyers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.weare.plantree.R;
+
+public class OnBording_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_bording);
+    }
+}
